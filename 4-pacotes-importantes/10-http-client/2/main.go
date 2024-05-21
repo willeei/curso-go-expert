ef4733bcd525e517370cdc98cf6a01e56c1bb88e
Williams Barriquero <williamsgomess@outlook.com>
2024-05-21 00:18:44 -0300

    feat: Add HTTP client functionality
    This commit adds functionality to make HTTP requests using the `net/http` package in Go. It includes two new files, `main.go` and `main_test.go`, that demonstrate how to use the `http.Client` struct to send GET and POST requests. The GET request retrieves the body of a webpage from "http://google.com" and prints it to the console. The POST request sends a JSON payload to "http://google.com" and copies the response body to the standard output. This will allow users to easily perform HTTP requests in their Go programs.

---
+++ 4-pacotes-importantes/10-http-client/2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	c := http.Client{Timeout: time.Second}
+	jsonVar := bytes.NewBuffer([]byte(`{"name":"Williams"}`))
+	resp, err := c.Post("http://google.com", "application/json", jsonVar)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.CopyBuffer(os.Stdout, resp.Body, nil)
+}
