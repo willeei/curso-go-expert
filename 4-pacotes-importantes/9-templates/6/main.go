69c04b4b47e61796b44ae1c2cac0fa51b794cce0
Williams Barriquero <williamsgomess@outlook.com>
2024-05-20 23:47:02 -0300

    Fundação
    Mapeando funções nos templates

---
+++ 4-pacotes-importantes/9-templates/6/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"os"
+	"strings"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	templates := []string{
+		"header.html",
+		"content.html",
+		"footer.html",
+	}
+	t := template.New("content.html")
+	t.Funcs(template.FuncMap{
+		"ToUpper": strings.ToUpper,
+	})
+	t = template.Must(t.ParseFiles(templates...))
+	err := t.Execute(os.Stdout, Cursos{
+		{"Go", 40},
+		{"Python", 45},
+		{"Java", 60},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
