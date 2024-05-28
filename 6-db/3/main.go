5805ab7a74904758d1a3d7646d609830faf40001
Williams Barriquero <williamsgomess@outlook.com>
2024-05-28 13:31:23 -0300

    Banco de dados
    Belongs to

---
+++ 6-db/3/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+}
+
+type Product struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	CategoryID int
+	Category   Category
+	gorm.Model
+}
+
+func main() {
+	dsn := "root:9655@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{})
+
+	category := Category{Name: "Eletronics"}
+	db.Create(&category)
+
+	db.Create(&Product{Name: "Laptop", Price: 1000, CategoryID: category.ID})
+
+	var products []Product
+	db.Preload("Category").Find(&products)
+	for _, product := range products {
+		fmt.Println(product.Name, product.Category.Name)
+	}
+}
