4dcc537562c07f95542ecb29ad14356715579e7b
Williams Barriquero <willbrr.dev@gmail.com>
2024-07-06 10:34:30 -0300

    Testando em batch

--- 8-Testing/1/tax_test.go
+++ 8-Testing/1/tax_test.go
@@ -2,13 +2,38 @@ package tax
 
 import "testing"
 
-func TextCalculateTax(t *testing.T) {
+func TestCalculateTax(t *testing.T) {
+	// Arange
 	amount := 500.0
 	expected := 5.0
 
+	// Act
 	result := CalculateTax(amount)
 
+	// Assert
 	if result != expected {
 		t.Errorf("CalculateTax(%f): expected %f, got %f", amount, expected, result)
+	}
+}
+
+func TestCalculateTaxBatch(t *testing.T) {
+	// Arange
+	type calcTax struct {
+		amount, expected float64
+	}
+
+	// Act
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+	}
+
+	// Assert
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expected {
+			t.Errorf("CalculateTax(%f): expected %f, got %f", item.amount, item.expected, result)
+		}
 	}
 }
