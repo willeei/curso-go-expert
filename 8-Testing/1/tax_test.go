f1614ab38402cf6b8dad64b0064128d00162ef82
Williams Barriquero <willbrr.dev@gmail.com>
2024-07-06 23:14:50 -0300

    Fuzzing

--- 8-Testing/1/tax_test.go
+++ 8-Testing/1/tax_test.go
@@ -39,6 +39,7 @@ func TestCalculateTaxBatch(t *testing.T) {
 	}
 }
 
+// Benchmarking
 func BenchmarkCalculateTax(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		CalculateTax(500.0)
@@ -49,4 +50,22 @@ func BenchmarkCalculateTax2(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		CalculateTax2(500.0)
 	}
+}
+
+// Fuzzing
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1500.0, 0.0}
+
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Receive %f but expected 0.0", result)
+		}
+		if amount > 20000 && result != 20 {
+			t.Errorf("Receive %f but expected 20.0", result)
+		}
+	})
 }
--- README.md
+++ README.md
@@ -14,3 +14,8 @@ exec: `go test -bench=.` com os Tests
 exec: `go test -bench=. -run=^#` apenas o Benchmark
 exec: `go test -bench=. -run=^# -count=10` com 10 chamadas
 exec: `go test -bench=. -run=^# -count=10 -benchtime=3s` executa por três 3s
+
+### Fuzzing
+
+Teste de mutação
+exec: `go test -fuzz=. -run=ˆ#`
