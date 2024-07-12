1b71a8839ec948ee6200d655f4baf8ab9d61d0d8
Williams Barriquero <willbrr.dev@gmail.com>
2024-07-11 23:35:40 -0300

    Criando FindAll com paginação

--- 9-API/internal/infra/database/product_db.go
+++ 9-API/internal/infra/database/product_db.go
@@ -17,6 +17,20 @@ func (p *Product) Create(product *entity.Product) error {
 	return p.DB.Create(product).Error
 }
 
+func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
+	var products []entity.Product
+	var err error
+	if sort == "" && sort != "asc" && sort != "desc" {
+		sort = "asc"
+	}
+	if page != 0 && limit != 0 {
+		err = p.DB.Offset((page - 1) * limit).Limit(limit).Order("created_at " + sort).Find(&products).Error
+	} else {
+		err = p.DB.Order("created_at " + sort).Find(&products).Error
+	}
+	return products, err
+}
+
 func (p *Product) FindByID(id string) (*entity.Product, error) {
 	var product entity.Product
 	err := p.DB.First(&product, "id = ?", id).Error
@@ -37,13 +51,4 @@ func (p *Product) Delete(id string) error {
 		return err
 	}
 	return p.DB.Delete(product).Error
-}
-
-func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
-	var products []entity.Product
-	err := p.DB.Offset((page - 1) * limit).Limit(limit).Order(sort).Find(&products).Error
-	if err != nil {
-		return nil, err
-	}
-	return products, nil
 }
