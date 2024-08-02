e3cd75acaa7171308ef7d836cadd62533f987e1d
Williams Barriquero <willbrr.dev@gmail.com>
2024-08-02 18:19:10 -0300

    feat: :sparkles: implements categories feature
    Persistindo Category via Playgroud
    Fazendo queries de Category

--- 13-GraphQL/cmd/server/server.go
+++ 13-GraphQL/cmd/server/server.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/willbrr.dev/goexpert/13-graphql/internal/database"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/willbrr.dev/goexpert/13-graphql/graph"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
