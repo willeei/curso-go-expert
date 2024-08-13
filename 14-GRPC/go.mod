03630eb0e313faa59e11003b48c0024e716eeb5c
Williams Barriquero <willbrr.dev@gmail.com>
2024-08-13 16:19:32 -0300

    feat: implemeta grpc

---
+++ 14-GRPC/go.mod
@@ -0,0 +1,16 @@
+module github.com/willbrr.dev/goexpert/14-grpc
+
+go 1.22.5
+
+require (
+	github.com/google/uuid v1.6.0
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
+)
---
+++ 14-GRPC/internal/database/category.go
@@ -0,0 +1,58 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+)
+
+type Category struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+}
+
+func NewCategory(db *sql.DB) *Category {
+	return &Category{db: db}
+}
+
+func (c *Category) Create(name, description string) (Category, error) {
+	id := uuid.New().String()
+	_, err := c.db.Exec(
+		"INSERT INTO categories (id, name, description) VALUES ($1, $2, $3)", id, name, description,
+	)
+	if err != nil {
+		return Category{}, err
+	}
+	return Category{ID: id, Name: name, Description: description}, nil
+}
+
+func (c *Category) FindAll() ([]Category, error) {
+	rows, err := c.db.Query("SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var categories []Category
+	for rows.Next() {
+		var category Category
+		err := rows.Scan(&category.ID, &category.Name, &category.Description)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+	return categories, nil
+}
+
+func (c *Category) FindByCourseID(courseID string) (Category, error) {
+	var category Category
+	err := c.db.QueryRow(
+		"SELECT c.id, c.name, c.description FROM categories c JOIN courses cr ON c.id = cr.category_id WHERE cr.id = $1",
+		courseID,
+	).Scan(&category.ID, &category.Name, &category.Description)
+	if err != nil {
+		return Category{}, err
+	}
+	return category, nil
+}
