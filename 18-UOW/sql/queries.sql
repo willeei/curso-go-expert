690f54dfa24d1a2e1bba444f832c95472c325d73
Williams Barriquero <willbrr.dev@gmail.com>
2024-08-26 09:06:14 -0300

    feat: :sparkles: unit of works

---
+++ 18-UOW/sql/queries.sql
@@ -0,0 +1,7 @@
+-- name: CreateCategory :exec
+INSERT INTO categories (id, name)
+VALUES (?, ?);
+
+-- name: CreateCourse :exec
+INSERT INTO courses (id, name, category_id)
+VALUES (?, ?, ?);
---
+++ 18-UOW/sql/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `categories` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `courses` (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    category_id INTEGER NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
